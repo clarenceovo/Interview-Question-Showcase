c1ded5b3bf9f64e6f57b516c3ec8ce5f7f549f44
clarenceovo <clarenceovo@gmail.com>
2022-06-17 13:15:31 +0800

    update testcase

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from app import main_function
 from util.test_base import set_keyboard_input,get_display_output
 import json
 
-
 def test_create_canvas():
     global test_result
     global test_input
@@ -99,3 +98,87 @@ def test_create_canvas_out_of_range_line_at_negative():
     output = get_display_output()
     assert output[2] == test_result['test_create_canvas_out_of_range_line_at_negative'][0]
     assert output[4] == test_result['test_create_canvas_out_of_range_line_at_negative'][1]
+
+def test_create_canvas_create_canva_wrong_input_type():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_create_canva_wrong_input_type'])
+    main_function()
+    output = get_display_output()
+    assert output[2] == test_result['test_create_canvas_create_canva_wrong_input_type'][0]
+
+def test_create_canvas_create_canva_missing_input():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_create_canva_missing_input'])
+    main_function()
+    output = get_display_output()
+    assert output[2] == test_result['test_create_canvas_create_canva_missing_input'][0]
+
+def test_create_canvas_add_line_wrong_input_type():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_add_line_wrong_input_type'])
+    main_function()
+    output = get_display_output()
+    assert output[4] == test_result['test_create_canvas_add_line_wrong_input_type'][0]
+
+
+def test_create_canvas_add_line_missing_input():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_add_line_missing_input'])
+    main_function()
+    output = get_display_output()
+    assert output[4] == test_result['test_create_canvas_add_line_missing_input'][0]
+
+
+def test_create_canvas_add_line_not_horizontal_nor_vertical_line_1():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_add_line_not_horizontal_nor_vertical_line_1'])
+    main_function()
+    output = get_display_output()
+    assert output[4] == test_result['test_create_canvas_add_line_not_horizontal_nor_vertical_line_1'][0]
+
+def test_create_canvas_add_line_not_horizontal_nor_vertical_line_2():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_add_line_not_horizontal_nor_vertical_line_2'])
+    main_function()
+    output = get_display_output()
+    assert output[4] == test_result['test_create_canvas_add_line_not_horizontal_nor_vertical_line_2'][0]
+
+
+def test_create_canvas_add_rectanlge_missing_input():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_add_rectanlge_missing_input'])
+    main_function()
+    output = get_display_output()
+    assert output[4] == test_result['test_create_canvas_add_rectanlge_missing_input'][0]
+
+def test_create_canvas_add_rectanlge_wrong_input_type():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_add_rectanlge_wrong_input_type'])
+    main_function()
+    output = get_display_output()
+    assert output[4] == test_result['test_create_canvas_add_rectanlge_wrong_input_type'][0]
+
+
+def test_create_canvas_fill_missing_input():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_fill_missing_input'])
+    main_function()
+    output = get_display_output()
+    assert output[6] == test_result['test_create_canvas_fill_missing_input'][0]
+
+def test_create_canvas_fill_wrong_input_type():
+    global test_result
+    global test_input
+    set_keyboard_input(test_input['test_create_canvas_fill_wrong_input_type'])
+    main_function()
+    output = get_display_output()
+    assert output[6] == test_result['test_create_canvas_fill_wrong_input_type'][0]
